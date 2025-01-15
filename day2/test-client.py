f898659650a5a5a40318cafbf69a4cdefbead2fd
akarapong <akarapong00123@gmaol.com>
2025-01-15 10:14:35 +0700

    Walkie Talkie

--- day2/test-client.py
+++ day2/test-client.py
@@ -1,9 +1,10 @@
 import socket
 
 HOST = '127.0.0.1'
-PORT = 21001
+PORT = 443
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("Connecting to server...")
     s.connect((HOST, PORT))
-    s.sendall(b'Hello from the client side!!')
+    s.sendall(b'Hello, world')
     data = s.recv(1024)
 print('Received', repr(data))
