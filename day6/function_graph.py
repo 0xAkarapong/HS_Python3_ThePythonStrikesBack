8a7bc242d20ad70519bcb1717548e9c436690540
akarapong <97168241+0xAkarapong@users.noreply.github.com>
2025-01-21 17:06:58 +0700

    x**2

--- day6/function_graph.py
+++ day6/function_graph.py
@@ -9,12 +9,21 @@ logger = logging.getLogger()
 logger.info("Program started")
 
 def f(x):
-    return x
+    return x ** 2
 
 pygame.init()
 
 w = 600
 h = 600
+
+x_min, x_max = -2, 2
+y_min, y_max = 0, 4
+
+x_scale = w / (x_max - x_min)
+y_scale = h / (y_max - y_min)
+
+x_origin = w // 2
+y_origin = h // 2
 
 screen = pygame.display.set_mode((w, h))
 pygame.display.set_caption("Draw graph")
@@ -29,10 +38,12 @@ while running:
 
     screen.fill((0, 0, 0))
 
-    for x in range(w):
+    for pixel_x in range(w):
+            x = x_min + pixel_x / x_scale
             y = f(x)
+            pixel_y = h - int((y - y_min) * y_scale)
             if y < h:
-                screen.set_at((x, h - y), (255, 255, 255))  # Invert y-axis for correct plotting
+                screen.set_at((pixel_x, pixel_y), (255, 255, 255)) 
 
 
     pygame.display.flip()
